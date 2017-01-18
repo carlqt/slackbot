cb45ad327d8b7b10644e655f598264c831984916
Carl Tablante <carl@guavapass.com>
2017-01-18 14:47:24 +0800

    deployed in google app engine. /hi endpoint

--- slack.go
+++ slack.go
@@ -1,11 +1,10 @@
-package main
+package slackbot
 
 import (
 	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
 )
 
@@ -27,29 +26,8 @@ type SlashResponse struct {
 	Text         string `json:"text"`
 }
 
-type TestP struct {
-	Token string
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
-	if err := json.NewEncoder(w).Encode(todos); err != nil {
-		panic(err)
-	}
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+func init() {
+	http.HandleFunc("/hi", Hi)
 }
 
 func Hi(w http.ResponseWriter, r *http.Request) {
