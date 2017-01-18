ed2c4700cb9abe901ffda7467408331bb8074c56
Carl Tablante <carl@guavapass.com>
2017-01-18 23:23:37 +0800

    setup token

--- slack.go
+++ slack.go
@@ -7,7 +7,10 @@ import (
 	"github.com/gorilla/schema"
 )
 
-const myToken = "hF5F1iTzGUUFWDI8gnS0JPIy"
+const hiToken = "hF5F1iTzGUUFWDI8gnS0JPIy"
+
+// const adviceToken = "1TfwAwnUZXMPY7rvvjT2aOVe"
+const adviceToken = "Hqq0RQzSPvMaZCnscvSOEr8D"
 
 type SlashForm struct {
 	Token       string `schema:"token"`
@@ -29,33 +32,41 @@ type SlashResponse struct {
 
 func init() {
 	hi := http.HandlerFunc(Hi)
+	badAdvice := http.HandlerFunc(BadAdviceHandler)
 
-	http.Handle("/hi", AuthHandler(hi))
-	http.HandleFunc("/bad_advice", BadAdviceHanlder)
+	http.Handle("/hi", AuthHandler(TokenHandler(hi, hiToken)))
+	http.Handle("/bad_advice", AuthHandler(TokenHandler(badAdvice, adviceToken)))
 }
 
 func AuthHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
-			form := &SlashForm{}
-			decoder := schema.NewDecoder()
-			r.ParseForm()
-			token := r.FormValue("token")
-
-			err := decoder.Decode(form, r.PostForm)
-			if err != nil {
-				return
-			}
-
-			if token != myToken {
-				w.WriteHeader(404)
-				w.Write([]byte("Invalid Token"))
-			}
 			next.ServeHTTP(w, r)
 		} else {
 			w.WriteHeader(404)
 			w.Write([]byte("bad request"))
 		}
+	})
+}
+
+func TokenHandler(next http.Handler, validToken string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := &SlashForm{}
+		decoder := schema.NewDecoder()
+		r.ParseForm()
+		token := r.FormValue("token")
+
+		err := decoder.Decode(form, r.PostForm)
+		if err != nil {
+			w.WriteHeader(404)
+			w.Write([]byte("Invalid Token"))
+		}
+
+		if token != validToken {
+			w.WriteHeader(404)
+			w.Write([]byte("Invalid Token"))
+		}
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -68,7 +79,7 @@ func Hi(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func BadAdviceHanlder(w http.ResponseWriter, r *http.Request) {
+func BadAdviceHandler(w http.ResponseWriter, r *http.Request) {
 	var response *SlashResponse
 	response = &SlashResponse{ResponseType: "in_channel", Text: BadAdvice()}
 
